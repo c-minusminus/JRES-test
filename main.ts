17900b6c9b46d81730b2c0edc75af7c84e2849ee
c-minusminus <mcross.student@bccspa.org>
2026-06-25 12:43:59 -0400

    Update pxt.json, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
 namespace customStrings {
     //% jres
-    export const helloWorld = hex``;
+    export const helloWorld = hex``
 }
 
-let res = "["
-for (let i = 0; i < customStrings.helloWorld.length; i++) {
-    res += customStrings.helloWorld[i] + ", "
-}
-res = res.slice(0, -2) + "]"
-console.log(res) // prints [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100]
+console.log(customStrings.helloWorld.toString())
